95d1d526e3efee177cbbd263becde9fa3aa575d0
sunil-495 <sunilsinghshekhawat495@gmail.com>
2026-02-17 13:35:10 +0530

    all oky

---
+++ modules/documentRequirement.model.js
@@ -0,0 +1,16 @@
+// models/DocRequirement.js
+import mongoose from "mongoose";
+
+const docRequirementSchema = new mongoose.Schema({
+    serviceId: { type: mongoose.Schema.Types.ObjectId, ref: 'Service', required: true },
+    planId: { type: mongoose.Schema.Types.ObjectId, ref: 'Plan', required: true },
+    requiredDocuments: [
+        {
+            docName: { type: String, required: true }, 
+            description: { type: String },          
+            isMandatory: { type: Boolean, default: true }
+        }
+    ]
+}, { timestamps: true });
+
+export const DocRequirement = mongoose.model("DocRequirement", docRequirementSchema);
